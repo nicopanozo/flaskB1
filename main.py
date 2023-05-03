2081ccad443cd0f058533db2703ac72454226a06
Nicolas Panozo <nicopanozoc@gmail.com>
2023-05-03 13:34:53 -0400

    basic project working

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(120))
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        user = User(name=name, email=email)
+        db.session.add(user)
+        db.session.commit()
+        return 'Thank you for submitting your details!'
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
